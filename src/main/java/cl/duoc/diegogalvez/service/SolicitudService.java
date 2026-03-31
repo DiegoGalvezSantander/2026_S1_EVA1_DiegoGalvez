d46193f88fc11083c2ba5b77e8b07d9d7b685c4a
Diego Galvez <di.galvezs@duocuc.cl>
2026-03-31 11:24:23 -0300

    Servicio Creado

--- src/main/java/cl/duoc/diegogalvez/service/SolicitudService.java
+++ src/main/java/cl/duoc/diegogalvez/service/SolicitudService.java
@@ -1,5 +1,57 @@
 package cl.duoc.diegogalvez.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+import cl.duoc.diegogalvez.model.SolicitudMedica;
+import cl.duoc.diegogalvez.repository.SolicitudRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class SolicitudService {
 
+    private final SolicitudRepository solicitudRepository;
+
+    public List<SolicitudMedica> obtenerTodas() {
+
+        return solicitudRepository.buscarTodas();
+    }
+
+    public Optional<SolicitudMedica> obtenerPorId(Long id) {
+        return solicitudRepository.buscarPorId(id);
+    
+    }
+
+    
+    public SolicitudMedica crear(SolicitudMedica solicitud) {
+        return solicitudRepository.guardar(solicitud);
+
+    }
+
+    public Optional<SolicitudMedica> actualizar(Long id, SolicitudMedica solicitud) {
+        if (solicitudRepository.existePorId(id)) {
+            solicitud.setId(id);
+            return Optional.of(solicitudRepository.guardar(solicitud));
+        }
+        return Optional.empty();
+    }
+
+    public boolean eliminar(Long id) {
+        if (solicitudRepository.existePorId(id)) {
+            solicitudRepository.eliminarPorId(id);
+            return true;
+        }
+        return false;
+    }
+
+
+    public List<SolicitudMedica> buscarPorPrioridad(String prioridad) {
+        return solicitudRepository.buscarTodas().stream()
+                .filter(s -> s.getPrioridad().equalsIgnoreCase(prioridad))
+                .collect(Collectors.toList());
+    }
 }
+
